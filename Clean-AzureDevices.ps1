8f1eb193e54511b3a8861724b331c02d2de82401
mwpatterson2000 <m.w.patterson@live.com>
2021-11-08 11:13:09 -0500

    Create Clean-AzureDevices.ps1

---
+++ Clean-AzureDevices.ps1
@@ -0,0 +1,18 @@
+﻿<#
+Name: Stefan Torres
+Documentation: 
+    1. https://docs.microsoft.com/en-us/azure/active-directory/devices/manage-stale-devices
+
+Purpose: To remove stale devices from AzureAD.
+
+#>
+
+# Set Dates For Disable & Delete
+$disableDate = (Get-Date).AddDays(-30)
+$deleteDate = (Get-Date).AddDays(-45)
+
+# Get Devices
+$devices = Get-AzureADDevice -All:$True
+
+# Devices to Disable
+# $devicesToDisable | Where {$_.ApproximateLastLogonTimeStamp -le $disableDate}
