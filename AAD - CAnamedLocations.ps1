addaa257e0c5e853deaeea5c14079f3a6db9fb24
mwpatterson2000 <m.w.patterson@live.com>
2022-01-28 13:46:44 -0500

    Update AAD - CAnamedLocations.ps1

--- AAD - CAnamedLocations.ps1
+++ AAD - CAnamedLocations.ps1
@@ -15,7 +15,9 @@ $exportFile = "C:\Temp\$(Get-Date -Format yyyy-MM-dd-HH-mm) - AAD - CA namedLoca
 #Get Conditional Access Policies   
 $tempAR = @(Get-AzureADMSNamedLocationPolicy)
 
-$Policies = @()
+# Build Array for output
+#$Script:NamedLocations = @()
+$Script:NamedLocations = [System.Collections.ArrayList]::new()
 
 # Build Export Array
 $count = @(($tempAR).Displayname).Count
@@ -24,7 +26,8 @@ for ($num = 0 ; $num -le $count ; $num++) {
     $tempIP = (($tempAR[$num]).IpRanges).cidrAddress -join ','
     $tempCR = (($tempAR[$num]).IpRanges).CountriesAndRegions -join ','
     $tempIUCR = (($tempAR[$num]).IpRanges).IncludeUnknownCountriesAndRegions -join ','
-    $Policies += New-Object PSobject -Property @{
+    #$Script:NamedLocations += New-Object PSobject -Property @{
+    $NamedLocationsT = New-Object PSobject -Property @{
         "Displayname"  = ($tempAR[$num]).displayName
         "isTrusted" = ($tempAR[$num]).isTrusted
         "ID"  = ($tempAR[$num]).id
@@ -32,7 +35,8 @@ for ($num = 0 ; $num -le $count ; $num++) {
         "CountriesAndRegions" = $tempCR
         "IncludeUnknownCountriesAndRegions" = $tempIUCR
     }
+    [void]$Script:NamedLocations.Add($NamedLocationsT)
 }
 
 # Export Data
-$Policies | export-csv -Path $exportFile -NoTypeInformation
+$Script:NamedLocations | export-csv -Path $exportFile -NoTypeInformation
